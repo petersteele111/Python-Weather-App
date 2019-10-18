303324aacdc9e340c82708c2ea6d2e8d739c86eb
Peter Steele <steelep@mail.nmc.edu>
2019-10-18 17:09:47 -0400

    Broke Out code into Methods

--- weather.py
+++ weather.py
@@ -19,37 +19,31 @@ class Weather:
         self.request_times = None
 
     def scrape(self):
-        r = requests.get("https://weather.com/weather/today/l/" + self.zipcode,
+        r = requests.get("https://weather.com/weather/today/l/" + "49690",
                          headers={'Cache-Control': 'no-cache'})
         self.soup = BeautifulSoup(r.content, 'html.parser')
 
-    def find_todays_weather(self):
-        self.location_name = self.soup.find('h1', class_='today_nowcard-location').text
-        self.today_feel_like = self.soup.find('span', class_='deg-feels').text
-        self.today_description = self.soup.find('div', class_='today_nowcard-phrase').text
-        self.today_precip = self.soup.find('span', class_='precip-val').text
-        self.today_short_description = self.soup.find('span', class_='today-wx-descrip').text
-        self.today_sunrise = self.soup.find('span', id='dp0-details-sunrise').text
-        self.today_sunset = self.soup.find('span', id='dp0-details-sunset').text
-        self.request_times = self.soup.find('p', class_='today_nowcard-timestamp').text
+    def find_todays_conditions(self):
         today_condition = self.soup.find('div', class_='today_nowcard-sidecar')
         for x in today_condition.find_all('td'):
             self.todays_conditions.append(x.text)
-        today_temp_high = self.soup.find('div', class_='today_nowcard-temp')
-        self.todays_temp.append(today_temp_high.text)
-        today_temp_low = self.soup.find('span', class_='deg-hilo-nowcard')
-        self.todays_temp.append(today_temp_low.next_sibling.next_sibling.next_sibling.text)
 
     def locations_name(self):
+        self.location_name = self.soup.find('h1', class_='today_nowcard-location').text
         return self.location_name
 
     def todays_high(self):
+        today_temp_high = self.soup.find('div', class_='today_nowcard-temp')
+        self.todays_temp.append(today_temp_high.text)
         return self.todays_temp[0]
 
     def todays_low(self):
+        today_temp_low = self.soup.find('span', class_='deg-hilo-nowcard')
+        self.todays_temp.append(today_temp_low.next_sibling.next_sibling.next_sibling.text)
         return self.todays_temp[1]
 
     def todays_feels_like(self):
+        self.today_feel_like = self.soup.find('span', class_='deg-feels').text
         return self.today_feel_like
 
     def todays_wind(self):
@@ -68,19 +62,32 @@ class Weather:
         return self.todays_conditions[4]
 
     def todays_description(self):
+        self.today_description = self.soup.find('div', class_='today_nowcard-phrase').text
         return self.today_description
 
     def todays_short_description(self):
+        self.today_short_description = self.soup.find('span', class_='today-wx-descrip').text
         return self.today_short_description
 
     def todays_precipitation(self):
+        self.today_precip = self.soup.find('span', class_='precip-val').text
         return self.today_precip
 
     def todays_sunrise(self):
+        self.today_sunrise = self.soup.find('span', id='dp0-details-sunrise').text
         return self.today_sunrise
 
     def todays_sunset(self):
+        self.today_sunset = self.soup.find('span', id='dp0-details-sunset').text
         return self.today_sunset
 
     def request_time(self):
+        self.request_times = self.soup.find('p', class_='today_nowcard-timestamp').text
         return self.request_times
+
+sault = Weather('49690')
+sault.scrape()
+sault.find_todays_conditions()
+print(sault.todays_feels_like())
+print(sault.locations_name())
+print(sault.todays_wind())
